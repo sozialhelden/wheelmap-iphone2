7ca674b8140489e32d9e776d4e380d5a63e3b150
Michael Thomas <michael.thomas@smartmobilefactory.com>
2012-12-06 11:44:28 +0100

    layout dashboard buttons

---
+++ Wheelmap/WMDashboardButton.m
@@ -0,0 +1,68 @@
+//
+//  WMDashboardButton.m
+//  Wheelmap
+//
+//  Created by Michael Thomas on 06.12.12.
+//  Copyright (c) 2012 Sozialhelden e.V. All rights reserved.
+//
+
+#import "WMDashboardButton.h"
+
+@implementation WMDashboardButton
+
+- (id)initWithFrame:(CGRect)frame andType:(WMDashboardButtonType)type
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+                
+        CGRect bounds = CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height);
+        
+        UIView *normalView = [[UIView alloc] initWithFrame:bounds];
+        
+        UIImageView *backgroundImageViewNormal = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"start_btn-grid.png"]];
+        backgroundImageViewNormal.frame = bounds;
+        [normalView addSubview:backgroundImageViewNormal];
+        
+        NSString *buttonImageStringNormal = @"";
+        
+        NSString *titleString = @"";
+        
+        switch (type) {
+            case WMDashboardButtonTypeNearby:
+                buttonImageStringNormal = @"start_icon-grid-locallist.png";
+                titleString = NSLocalizedString(@"IN DER NÄHE", nil);
+                break;
+            case WMDashboardButtonTypeMap:
+                buttonImageStringNormal = @"start_icon-grid-map.png";
+                titleString = NSLocalizedString(@"KARTE", nil);
+                break;
+            case WMDashboardButtonTypeCategories:
+                buttonImageStringNormal = @"start_icon-grid-category.png";
+                titleString = NSLocalizedString(@"KATEGORIEN", nil);
+                break;
+            case WMDashboardButtonTypeHelp:
+                buttonImageStringNormal = @"start_icon-grid-help.png";
+                titleString = NSLocalizedString(@"MITHELFEN", nil);
+                break;
+            default:
+                break;
+        }
+        
+        UIImage *imageNormal = [UIImage imageNamed:buttonImageStringNormal];
+        UIImageView *imageViewNormal = [[UIImageView alloc] initWithImage:imageNormal];
+        imageViewNormal.frame = CGRectMake( (frame.size.width - imageNormal.size.width)/2, (frame.size.height - imageNormal.size.height)/2 - 10.0f, imageNormal.size.width, imageNormal.size.height);
+        [normalView addSubview:imageViewNormal];
+        
+        WMLabel *titleLabelNormal = [[WMLabel alloc] initWithFrame:CGRectMake(10.0f, bounds.size.height - 35.0f, bounds.size.width - 20.0f, 30.0f)];
+        titleLabelNormal.textColor = [UIColor colorWithRed:39.0f/255.0f green:54.0f/255.0f blue:69.0f/255.0f alpha:1.0f];
+        titleLabelNormal.text = titleString;
+        titleLabelNormal.fontSize = 12.0f;
+        titleLabelNormal.fontType = kWMLabelFontTypeBold;
+        [normalView addSubview:titleLabelNormal];
+                                     
+        [self setView:normalView forControlState:UIControlStateNormal];
+    }
+    return self;
+}
+
+@end
