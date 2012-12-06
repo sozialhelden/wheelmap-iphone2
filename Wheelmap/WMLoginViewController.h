11a791e5b9b3a62fdd530888e22a068a5525938f
Michael Thomas <michael.thomas@smartmobilefactory.com>
2012-12-06 15:06:47 +0100

    added button resources

---
+++ Wheelmap/WMLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  WMLoginViewController.h
+//  Wheelmap
+//
+//  Created by Michael Thomas on 06.12.12.
+//  Copyright (c) 2012 Sozialhelden e.V. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMLoginViewController : WMViewController
+
+@end
---
+++ Wheelmap/WMLoginViewController.m
@@ -0,0 +1,40 @@
+//
+//  WMLoginViewController.m
+//  Wheelmap
+//
+//  Created by Michael Thomas on 06.12.12.
+//  Copyright (c) 2012 Sozialhelden e.V. All rights reserved.
+//
+
+#import "WMLoginViewController.h"
+
+@implementation WMLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];	
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+    [self.navigationController setToolbarHidden:YES animated:YES];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
