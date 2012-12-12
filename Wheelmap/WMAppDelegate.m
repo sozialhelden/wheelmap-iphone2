dca620dadc8bc43b9fe66b28a15cf8775f4acdb8
npngseja <taehun.kim2@gmail.com>
2012-12-12 12:31:07 +0100

    added Hockey SDK without beta/release identifiers.

--- Wheelmap/WMAppDelegate.m
+++ Wheelmap/WMAppDelegate.m
@@ -7,12 +7,23 @@
 //
 
 #import "WMAppDelegate.h"
+#import <HockeySDK/HockeySDK.h>
+
+@interface WMAppDelegate (HockeySDK) <BITHockeyManagerDelegate, BITUpdateManagerDelegate, BITCrashManagerDelegate>
+
+@end
 
 @implementation WMAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
+    [[BITHockeyManager sharedHockeyManager] configureWithBetaIdentifier:@"BETA_IDENTIFIER"
+                                                         liveIdentifier:@"LIVE_IDENTIFIER"
+                                                               delegate:self];
+    [[BITHockeyManager sharedHockeyManager] startManager];
+    
     return YES;
 }
 							
@@ -41,6 +52,15 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+#pragma mark - BITUpdateManagerDelegate
+- (NSString *)customDeviceIdentifierForUpdateManager:(BITUpdateManager *)updateManager {
+#ifndef CONFIGURATION_AppStore
+    if ([[UIDevice currentDevice] respondsToSelector:@selector(uniqueIdentifier)])
+        return [[UIDevice currentDevice] performSelector:@selector(uniqueIdentifier)];
+#endif
+    return nil;
 }
 
 @end
