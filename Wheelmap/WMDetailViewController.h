1669ee224885a91ab972eb622053c623fc0ab5bd
AndreaGer <missgerlach@googlemail.com>
2012-12-14 00:55:49 +0100

    map animation added

--- Wheelmap/WMDetailViewController.h
+++ Wheelmap/WMDetailViewController.h
@@ -16,48 +16,66 @@
 
 @interface WMDetailViewController : WMViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate, MKMapViewDelegate, UIActionSheetDelegate, CLLocationManagerDelegate>
 
-@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
-@property (nonatomic, strong) UIScrollView *imageScrollView;
-@property (nonatomic, strong) MKMapView *mapView;
-@property (nonatomic, strong) UIImagePickerController *imagePicker;
-@property (nonatomic, strong) NSMutableArray *imageViewsInScrollView;
-@property (nonatomic, strong) NSMutableArray *imageURLArray;
-@property (nonatomic, strong) UIView *contactInfoView;
-
-@property (assign) int gabIfStatusUnknown;
 @property (nonatomic) Node *node;
-@property (nonatomic, strong) UIImage *accessImage;
-@property (nonatomic, strong) NSString *wheelchairAccess;
-@property (nonatomic, strong) UIView *fourButtonView;
-@property (nonatomic, strong) MKAnnotationView *annotationView;
 
-@property (nonatomic, strong) UIImageView *compassView;
-@property (nonatomic, assign) int startY;
-@property (nonatomic, assign) int start;
-@property (nonatomic, assign) int gab;
+@property (nonatomic, strong) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) UIView *mainView;
 
-@property (nonatomic, strong) WMMapAnnotation *annotation;
+//// 2 MAIN COMPONENTS OF THE MAIN VIEW
 
-@property (nonatomic, strong) UILabel *headingLabel;
+//MAP VIEW
+@property (nonatomic, strong) MKMapView *mapView;
+@property (assign) BOOL mapViewOpen;
+//CONTENT VIEW
+@property (nonatomic, strong) UIView *contentView;
+
+//// PARTS OF THE CONTENT VIEW
+
+// MAIN INFO VIEW
+@property (nonatomic, strong) UIView *mainInfoView;
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *nodeTypeLabel;
+// WHEEL ACCESS AND ASK FRIENDS BUTTON VIEW
+@property (nonatomic, strong) UIView *wheelAccessView;
+@property (nonatomic, strong) UIButton *wheelAccessButton;
+@property (nonatomic, strong) UIImage *accessImage;
+@property (nonatomic, strong) NSString *wheelchairAccess;
+@property (nonatomic, strong) UIButton *askFriendsButton;
+@property (assign) int gabIfStatusUnknown;
+// CONTACT INFO VIEW
+@property (nonatomic, strong) UIView *contactInfoView;
 @property (nonatomic, strong) UILabel *streetLabel;
 @property (nonatomic, strong) UILabel *postcodeAndCityLabel;
 @property (nonatomic, strong) UITextView *websiteLabel;
 @property (nonatomic, strong) UITextView *phoneLabel;
 @property (nonatomic, strong) UILabel *distanceLabel;
-
-@property (nonatomic, strong) UIButton *wheelAccessButton;
-@property (nonatomic, strong) UIButton *moreInfoButton;
-@property (nonatomic, strong) UIButton *callButton;
-@property (nonatomic, strong) UIButton *websiteButton;
-@property (nonatomic, strong) UIButton *commentButton;
-@property (nonatomic, strong) UIButton *naviButton;
+@property (nonatomic, strong) UIImageView *compassView;
+// IMAGESCROLLVIEW
+@property (nonatomic, strong) UIScrollView *imageScrollView;
+@property (nonatomic, strong) UIImagePickerController *imagePicker;
+@property (nonatomic, strong) NSMutableArray *imageViewsInScrollView;
+@property (nonatomic, strong) NSMutableArray *imageURLArray;
+@property (nonatomic, assign) int start;
+@property (nonatomic, assign) int gab;
+// ADDITIONALINFOVIEW
+@property (nonatomic, strong) UIView *additionalButtonView;
 @property (nonatomic, strong) UIButton *shareLocationButton;
-@property (nonatomic, strong) UIButton *askFriendsButton;
+@property (nonatomic, strong) UIButton *moreInfoButton;
+@property (nonatomic, strong) UIButton *naviButton;
+@property (assign) int threeButtonWidth;
+
+
+
+
 
 @property (nonatomic) CLLocationCoordinate2D poiLocation;
 @property (nonatomic, strong) MKUserLocation *currentLocation;
+@property (nonatomic, strong) MKAnnotationView *annotationView;
+@property (nonatomic, strong) WMMapAnnotation *annotation;
+
+@property (nonatomic, assign) int startY;
+@property (nonatomic, strong) UIButton *enlargeMapButton;
+
 
 
 
