79725f057aec54d01810ea87a4f63b4628848bb7
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2015-10-06 15:00:28 +0200

    Fix NSInvalidArgumentException in - [WMEditPOIViewController dataManager:didUpdateNode:], line 432

--- Wheelmap/WMNavigationControllerBase.h
+++ Wheelmap/WMNavigationControllerBase.h
@@ -53,8 +53,6 @@ UINavigationControllerDelegate, UIAlertViewDelegate>
 - (void)resetMapAndListToNormalUseCase;
 - (void)mapWasMoved;
 
-- (void)addTemporaryNode:(Node *)node;
-
 //-(void)updateNodesNear:(CLLocationCoordinate2D)coord;
 //-(void)updateNodesWithoutLoadingWheelNear:(CLLocationCoordinate2D)coord;
 -(void)updateNodesWithRegion:(MKCoordinateRegion)region;
