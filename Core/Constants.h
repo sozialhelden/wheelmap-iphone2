7914319fd4d49b6700f91b63e269a890075c6c45
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2015-10-28 09:54:08 +0100

    Remove unused "WMCategoryListViewControler" class

--- Core/Constants.h
+++ Core/Constants.h
@@ -20,7 +20,6 @@
 #define ODBL_URL				@"http://opendatacommons.org/licenses/odbl/"
 
 #define IS_OS_8_OR_LATER		([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
-#define ODBL_URL				@"http://opendatacommons.org/licenses/odbl/"
 
 // WMConfig
 
