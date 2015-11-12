f8143b3f22564d2f0bbf2d1e9397289035429d72
Hans Seiffert <hans.seiffert@smartmobilefactory.com>
2015-11-12 14:40:11 +0100

    Complete auto-layout usage in the WMCreditsViewController

--- Core/Controller/Credit/WMCreditsViewController.h
+++ Core/Controller/Credit/WMCreditsViewController.h
@@ -8,13 +8,4 @@
 
 @interface WMCreditsViewController : WMViewController
 
-@property (nonatomic,strong) IBOutlet UIView* containerView;
-
-@property (nonatomic, strong) IBOutlet WMButton *doneButton;
-@property (nonatomic, strong) IBOutlet WMLabel *titleLabel;
-@property (nonatomic, strong) IBOutlet UIScrollView *scroller;
-@property (weak, nonatomic) IBOutlet UIView *navigationBar;
-
-- (IBAction)donePressed:(id)sender;
-
 @end
